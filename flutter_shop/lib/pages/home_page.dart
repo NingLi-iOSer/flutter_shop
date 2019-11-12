87d722777f2dc66b2d227106da5f99108b1ee17a
lining <lining201702@126.com>
2019-11-12 15:40:37 +0800

    首页轮播图

--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_shop/service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -13,13 +16,15 @@ class _HomePageState extends State<HomePage> {
 
   TextEditingController typeController = TextEditingController();
   String homeContent = '暂未加载首页数据';
+  // banner 数据
+  List swiperDataList;
 
   @override
   void initState() {
     super.initState();
-    loadHomePageContent();
+    // loadHomePageContent();
   }
-
+  
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -29,7 +34,24 @@ class _HomePageState extends State<HomePage> {
         ),
         body: Container(
           child: SingleChildScrollView(
-            child: Text(homeContent),
+            child: FutureBuilder(
+              future: getHomePageContent(),
+              builder: (context, snapShot) {
+                if (snapShot.hasData) {
+                  var data = json.decode(snapShot.data.toString());
+                  List<Map> swiper = (data['data']['slides'] as List).cast();
+                  return Column(
+                    children: <Widget>[
+                      SwiperDIY(swiperDataList: swiper),
+                    ],
+                  );
+                } else {
+                  return Center(
+                    child: Text('Loading...'),
+                  );
+                }
+              },
+            ),
           )
         )
       )
@@ -42,5 +64,28 @@ class _HomePageState extends State<HomePage> {
         homeContent = value.toString();
       });
     });
+  }
+}
+
+class SwiperDIY extends StatelessWidget {
+  const SwiperDIY({Key key, this.swiperDataList}) : super(key: key);
+
+  final List swiperDataList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      child: Swiper(
+        itemBuilder: (context, index) {
+          return Image.network(swiperDataList[index]['image'].toString(), fit: BoxFit.fill,);
+        },
+        itemCount: swiperDataList.length,
+        autoplay: true,
+        pagination: SwiperPagination(),
+        scale: 0.75,
+        duration: 1000,
+      ),
+    );
   }
 }
